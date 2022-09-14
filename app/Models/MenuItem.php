f51c0c9c85aa9eab287184585d06c799c2edd99f
Tathod <rahul.tathod@metro-gsc.in>
2022-09-14 19:16:17 +0530

    changes

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
+
+class MenuItem extends Model
+{
+    // use HasFactory;
+    // protected $parentColumn = 'parent_id';
+
+    // public function parent()
+    // {
+    //     return $this->belongsTo(MenuItem::class,$this->parentColumn);
+    // }
+
+    // public function children()
+    // {
+    //     return $this->hasMany(MenuItem::class, $this->parentColumn);
+    // }
+
+    // public function allChildren()
+    // {
+    //     return $this->children()->with('allChildren');
+    // }
+
+}
